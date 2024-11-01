73a1f21cf2eb2fc8df323aa6a5408517f511f7d9
David Chen <david.chen@earthscope.org>
2024-11-01 13:18:43 -0700

    finish main implementation for init-reverse

--- initial-reverse/main.c
+++ initial-reverse/main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+
+typedef struct node {
+    char value[256];
+    struct node* next;
+    struct node* prev;
+} Node;
+
+void freeall(Node* root) {
+    Node* current = root;
+    while (current != NULL) {
+        Node* tmp = current;
+        current = current->next;
+        free(tmp);
+    }
+}
+
 int main(int argc, char * argv[]) {
     int num_args = argc - 1;
-    char* inputFile;
-    char* outputFile;
+    char* inputFile = NULL;
+    char* outputFile = NULL;
+    FILE* fp_out = NULL;
+
     if (num_args > 2) {
         fprintf(stderr, "usage: reverse <input> <output>\n");
         return 1;
@@ -11,19 +32,11 @@ int main(int argc, char * argv[]) {
     else if (num_args == 2) {
         inputFile = argv[1];
         outputFile = argv[2];
+        fp_out = fopen(outputFile, "w");
     }
     else if (num_args == 1) {
-        // TODO: Need to seek end of file and work our way backwards
         inputFile = argv[1];
-        FILE* fp = fopen(inputFile, "r");
-        if (fp == NULL) {
-            fprintf(stderr, "reverse: cannot open file '%s'\n", inputFile);
-            return 1;
-        }
-//        while ((read = getline(&line, &len, fp)) != -1) {
-//            printf("Retrieved line of length %zu:\n", read);
-//            printf("%s", line);
-//        }
+        fp_out = stdout;
     }
     else {
         char input[1000];
@@ -31,5 +44,41 @@ int main(int argc, char * argv[]) {
             printf("%s", input);
         }
     }
+
+    FILE* fp_in = fopen(inputFile, "r");
+    if (fp_in == NULL) {
+        fprintf(stderr, "reverse: cannot open file '%s'\n", inputFile);
+        return 1;
+    }
+
+    char buffer[256];
+    Node* root = (Node*) malloc(sizeof(Node));
+    root->next = NULL;
+    root->prev = NULL;
+    Node* current = root;
+    assert(sizeof(current->value) >= sizeof(buffer));
+
+    // Insert into linked list
+    while (fgets(buffer, sizeof buffer, fp_in) != NULL) {
+        strcpy(current->value, buffer);
+        Node* new = (Node*) malloc(sizeof(Node));
+        current->next = new;
+        new->next = NULL;
+        new->prev = current;
+        current = new;
+    }
+
+    // Read in reverse
+    while (current != NULL) {
+        fprintf(fp_out, "%s", current->value);
+        current = current->prev;
+    }
+
+    // Cleanup
+    fclose(fp_in);
+    if (outputFile) {
+        fclose(fp_out);
+    }
+    freeall(root);
     return 0;
 }
